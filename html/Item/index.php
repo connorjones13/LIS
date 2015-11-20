1eadbeedcbbf71d1dc513b3873c390b1a2bba5b4
Connor Jones <connorjones135@gmail.com>
2015-11-19 19:53:55 -0600

    item page styled

--- html/Item/index.php
+++ html/Item/index.php
@@ -1,34 +1,46 @@
 <?php
 	require_once(__DIR__ . "/../../includes/LIS/autoload.php");
 	$pdo = new \LIS\Database\PDO_MySQL();
-	$item = new LIS\RentalItem\Book($this->_pdo);
-	$controller = new \LIS\Controllers\CreateUserController($pdo);
+$controller = new \LIS\Controllers\CreateUserController($pdo);
+$item = new LIS\RentalItem\Book($pdo);
 
-	$page_title = "$item->getTitle()";
+// this for testing. todo: remove when page is passed a item
+$item->create("This is a book summary.", "My First Book", "Horror",
+	DateTime::createFromFormat("m-d-Y", "11-7-2015"), 2, "", "", ["Jim", "Bob", "Billy"]);
+$authors = array("John", "Jacob", "Jacky");
+// todo: remove above section
+
+$page_title = $item->getTitle();
 
 ?>
 
 <!doctype html>
 <html lang="en">
 <head>
-	<?php require_once(__DIR__ . "/head.php"); ?>
+	<?php require_once(__DIR__ . "/../../includes/html_templates/head.php"); ?>
 </head>
 <body>
 <header class="navbar navbar-fixed-top navbar-inverse">
-	<?php require_once(__DIR__ . "/header.php"); ?>
+	<?php require_once(__DIR__ . "/../../includes/html_templates/header.php"); ?>
 </header>
 <div class="content">
-	<div class="pull-left">
-		<img src="http://lorempixel.com/400/400/cats/">
-	</div>
-	<div class="pull-right">
-		<h1><?= $item->getTitle() ?></h1>
-		<h4><?= $item->getCategory() ?></h4>
-		<p><?= $item->getSummary() ?></p>
+	<div class="container ip-container">
+		<div class="pull-left ip-img">
+			<img src="http://lorempixel.com/400/400/">
+		</div>
+		<div class="">
+			<h1><?= $item->getTitle() ?></h1>
+			<h4><?= $item->getCategory() ?></h4>
+			<p><?= $item->getSummary() ?></p>
+			<!-- if book -->
+			<p>Authors: <?= implode(", ", $item->getAuthors()); ?></p>
+			<!--todo: if logged in, show reserve button -->
+			<a href="#" class="btn btn-lg btn-primary">Reserve</a>
+		</div>
 	</div>
 </div>
 <footer class="footer">
-	<?php require_once(__DIR__ . "/footer.php"); ?>
+	<?php require_once(__DIR__ . "/../../includes/html_templates/footer.php"); ?>
 </footer>
 </body>
 </html>
