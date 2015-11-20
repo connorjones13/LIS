7ee1c06f18b9cb48d5e399528cedcccd35567798
Connor Jones <connorjones135@gmail.com>
2015-11-19 19:57:51 -0600

    added const TYPE

--- includes/LIS/RentalItem/Magazine.php
+++ includes/LIS/RentalItem/Magazine.php
@@ -6,6 +6,8 @@
 
 	class Magazine extends RentalItem {
 
+		const TYPE = 2;
+		//todo: insert type into DB
 		protected $publication, $issue_number;
 
 		public function create($summary, $title, $category, $date_published, $status, $publication = "", $issue_number = 0) {
