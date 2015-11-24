43126b2fadc7feab686e5a63dad3cbbb252104c1
Steven Imle (MacBook) <stevenimle@gmail.com>
2015-11-23 23:47:52 -0600

    Cleaned up browse view

--- html/browse/index.php
+++ html/browse/index.php
@@ -19,14 +19,11 @@ $page_title = "Browse Rental Items";
 	<div class="container-fluid">
 		<div class="row">
 			<div class="text-center"><h1>Browse Available Rental Items</h1></div>
-
 			<div class="col-lg-8 col-md-8 col-sm-10 col-lg-offset-2 col-md-offset-2 col-sm-offset-1">
 				<h2>Books</h2>
-				<?php
-				$book = new \LIS\RentalItem\Book($pdo);
-				$books = $book->getAllByStatus($pdo, 0);
-				foreach ($books as $book) { ?>
-
+				<?php $book = new \LIS\RentalItem\Book($pdo); ?>
+				<?php $books = $book->getAllByStatus($pdo, 0); ?>
+				<?php foreach ($books as $book) { ?>
 					<div class="col-sm-6 col-md-3 list_items">
 						<h4><b><a href="/item/<?= $book->getId() ?>/"><?= $book->getTitle(); ?></a></b></h4>
 
@@ -39,47 +36,42 @@ $page_title = "Browse Rental Items";
 						<p>Authors: <?= implode(", ", \LIS\RentalItem\Author::findAllForBook($pdo, $book)); ?></p>
 					</div>
 				<?php } ?>
-				</div>
-				<div class="center col-lg-8 col-md-8 col-sm-10 col-lg-offset-2 col-md-offset-2 col-sm-offset-1">
-					<h2>Magazines</h2>
-					<?php
-					$magazine = new \LIS\RentalItem\Magazine($pdo);
-					$magazines = $magazine->getAllByStatus($pdo, 0);
-					foreach ($magazines as $magazine) { ?>
+			</div>
+			<div class="center col-lg-8 col-md-8 col-sm-10 col-lg-offset-2 col-md-offset-2 col-sm-offset-1">
+				<h2>Magazines</h2>
+				<?php $magazine = new \LIS\RentalItem\Magazine($pdo); ?>
+				<?php $magazines = $magazine->getAllByStatus($pdo, 0); ?>
+				<?php foreach ($magazines as $magazine) { ?>
+					<div class="col-sm-6 col-md-3 list_items">
+						<h4><b><a href="/item/<?= $magazine->getId() ?>/"><?= $magazine->getTitle(); ?></a></b></h4>
 
-						<div class="col-sm-6 col-md-3 list_items">
-							<h4><b><a href="/item/<?= $magazine->getId() ?>/"><?= $magazine->getTitle(); ?></a></b></h4>
+						<p>Category: <?= $magazine->getCategory(); ?></p>
 
-							<p>Category: <?= $magazine->getCategory(); ?></p>
+						<p>Summary: <?= $magazine->getSummary(); ?></p>
 
-							<p>Summary: <?= $magazine->getSummary(); ?></p>
+						<p>Date Published: <?= $magazine->getDatePublished()->format("m-d-Y"); ?></p>
 
-							<p>Date Published: <?= $magazine->getDatePublished()->format("m-d-Y"); ?></p>
+						<p>Publication: <?= $magazine->getPublication(); ?></p>
+					</div>
+				<?php } ?>
+			</div>
+			<div class="center col-lg-8 col-md-8 col-sm-10 col-lg-offset-2 col-md-offset-2 col-sm-offset-1">
+				<h2>DVDs</h2>
+				<?php $dvd = new \LIS\RentalItem\DVD($pdo); ?>
+				<?php $dvds = $dvd->getAllByStatus($pdo, 0); ?>
+				<?php foreach ($dvds as $dvd) { ?>
+					<div class="col-sm-6 col-md-3 list_items">
+						<h4><b><a href="/item/<?= $dvd->getId() ?>/"><?= $dvd->getTitle(); ?></a></b></h4>
 
-							<p>Publication: <?= $magazine->getPublication(); ?></p>
-						</div>
-					<?php } ?>
-				</div>
-				<div class="center col-lg-8 col-md-8 col-sm-10 col-lg-offset-2 col-md-offset-2 col-sm-offset-1">
-					<h2>DVDs</h2>
-					<?php
-					$dvd = new \LIS\RentalItem\DVD($pdo);
-					$dvds = $dvd->getAllByStatus($pdo, 0);
-					foreach ($dvds as $dvd) { ?>
+						<p>Category: <?= $dvd->getCategory(); ?></p>
 
-						<div class="col-sm-6 col-md-3 list_items">
-							<h4><b><a href="/item/<?= $dvd->getId() ?>/"><?= $dvd->getTitle(); ?></a></b></h4>
+						<p>Summary: <?= $dvd->getSummary(); ?></p>
 
-							<p>Category: <?= $dvd->getCategory(); ?></p>
+						<p>Date Published: <?= $dvd->getDatePublished()->format("m-d-Y"); ?></p>
 
-							<p>Summary: <?= $dvd->getSummary(); ?></p>
-
-							<p>Date Published: <?= $dvd->getDatePublished()->format("m-d-Y"); ?></p>
-
-							<p>Director: <?= $dvd->getDirector(); ?></p>
-						</div>
-					<?php } ?>
-				</div>
+						<p>Director: <?= $dvd->getDirector(); ?></p>
+					</div>
+				<?php } ?>
 			</div>
 		</div>
 	</div>
