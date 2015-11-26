882fb26d696f0862c2462970656cb4013e1cf3eb
Steven Imle (MacBook) <stevenimle@gmail.com>
2015-11-25 21:36:25 -0600

    I'm not proud of what I've done here...

--- html/controlpanel/index.php
+++ html/controlpanel/index.php
@@ -19,11 +19,6 @@
 <body>
 <header class="navbar navbar-fixed-top navbar-inverse">
 	<?php require_once(__DIR__ . "/../../includes/html_templates/header.php"); ?>
-	<script type="text/javascript">
-		$(".show_data").click(function() {
-
-		});
-	</script>
 </header>
 <div class="content">
 	<div class="container-fluid">
@@ -34,22 +29,22 @@
 				<h1 class="page-header">Control Panel</h1>
 
 				<div class="row placeholders">
-					<div class="col-xs-6 col-sm-3 placeholder show_data" data-view="available">
+					<div class="col-xs-6 col-sm-3 placeholder show_data" data-view="available" data-title="Available">
 						<h2><?= \LIS\Utility::formatNumber(\LIS\RentalItem\RentalItem::getAvailableItemCount($pdo)) ?></h2>
 						<h4>Available Items</h4>
 						<span class="text-muted">Click to View</span>
 					</div>
-					<div class="col-xs-6 col-sm-3 placeholder show_data" data-view="checked_out">
+					<div class="col-xs-6 col-sm-3 placeholder show_data" data-view="checked_out" data-title="Checked Out">
 						<h2><?= \LIS\Utility::formatNumber(\LIS\Checkout::getItemCheckedOutCount($pdo)) ?></h2>
 						<h4>Checked Out Items</h4>
 						<span class="text-muted">Click to View</span>
 					</div>
-					<div class="col-xs-6 col-sm-3 placeholder show_data" data-view="damaged">
+					<div class="col-xs-6 col-sm-3 placeholder show_data" data-view="damaged" data-title="Damaged">
 						<h2><?= \LIS\Utility::formatNumber(\LIS\RentalItem\RentalItem::getDamagedItemCount($pdo)) ?></h2>
 						<h4>Damaged Items</h4>
 						<span class="text-muted">Click to View</span>
 					</div>
-					<div class="col-xs-6 col-sm-3 placeholder show_data" data-view="lost">
+					<div class="col-xs-6 col-sm-3 placeholder show_data" data-view="lost" data-title="Lost">
 						<h2><?= \LIS\Utility::formatNumber(\LIS\RentalItem\RentalItem::getLostItemCount($pdo)) ?></h2>
 						<h4>Lost Items</h4>
 						<span class="text-muted">Click to View</span>
@@ -58,17 +53,16 @@
 
 				<h2 class="sub-header">Available Items</h2>
 				<div class="table-responsive">
-					<table class="table table-striped">
+					<table class="table table-striped" id="view_table">
 						<thead>
 							<tr>
 								<th>ID#</th>
 								<th>Title</th>
 								<th>Category</th>
 								<th>Creators</th>
-								<th></th>
 							</tr>
 						</thead>
-						<tbody>
+						<tbody id="available">
 							<?php foreach (\LIS\RentalItem\RentalItem::getAllAvailable($pdo) as $item) { ?>
 								<tr>
 									<td><?= \LIS\Utility::formatNumber($item->getId()) ?></td>
@@ -83,7 +77,60 @@
 											<?= $item->getPublication() ?> (#<?= $item->getIssueNumber() ?>)
 										<?php } ?>
 									</td>
-									<td></td>
+								</tr>
+							<?php } ?>
+						</tbody>
+						<tbody id="checked_out" style="display: none;">
+							<?php foreach (\LIS\RentalItem\RentalItem::getAllCheckedOut($pdo) as $item) { ?>
+								<tr>
+									<td><?= \LIS\Utility::formatNumber($item->getId()) ?></td>
+									<td><?= $item->getTitle() ?></td>
+									<td><?= $item->getCategory() ?></td>
+									<td>
+										<?php if ($item->isBook()) { ?>
+											<?= implode(", ", \LIS\RentalItem\Author::findAllForBook($pdo, $item)) ?>
+										<?php } else if ($item->isDVD()) { ?>
+											<?= $item->getDirector() ?>
+										<?php } else if ($item->isMagazine()) { ?>
+											<?= $item->getPublication() ?> (#<?= $item->getIssueNumber() ?>)
+										<?php } ?>
+									</td>
+								</tr>
+							<?php } ?>
+						</tbody>
+						<tbody id="damaged" style="display: none;">
+							<?php foreach (\LIS\RentalItem\RentalItem::getAllDamaged($pdo) as $item) { ?>
+								<tr>
+									<td><?= \LIS\Utility::formatNumber($item->getId()) ?></td>
+									<td><?= $item->getTitle() ?></td>
+									<td><?= $item->getCategory() ?></td>
+									<td>
+										<?php if ($item->isBook()) { ?>
+											<?= implode(", ", \LIS\RentalItem\Author::findAllForBook($pdo, $item)) ?>
+										<?php } else if ($item->isDVD()) { ?>
+											<?= $item->getDirector() ?>
+										<?php } else if ($item->isMagazine()) { ?>
+											<?= $item->getPublication() ?> (#<?= $item->getIssueNumber() ?>)
+										<?php } ?>
+									</td>
+								</tr>
+							<?php } ?>
+						</tbody>
+						<tbody id="lost" style="display: none;">
+							<?php foreach (\LIS\RentalItem\RentalItem::getAllLost($pdo) as $item) { ?>
+								<tr>
+									<td><?= \LIS\Utility::formatNumber($item->getId()) ?></td>
+									<td><?= $item->getTitle() ?></td>
+									<td><?= $item->getCategory() ?></td>
+									<td>
+										<?php if ($item->isBook()) { ?>
+											<?= implode(", ", \LIS\RentalItem\Author::findAllForBook($pdo, $item)) ?>
+										<?php } else if ($item->isDVD()) { ?>
+											<?= $item->getDirector() ?>
+										<?php } else if ($item->isMagazine()) { ?>
+											<?= $item->getPublication() ?> (#<?= $item->getIssueNumber() ?>)
+										<?php } ?>
+									</td>
 								</tr>
 							<?php } ?>
 						</tbody>
@@ -95,6 +142,13 @@
 </div>
 <footer class="footer">
 	<?php require_once(__DIR__ . "/../../includes/html_templates/footer.php"); ?>
+	<script type="text/javascript">
+		$(".show_data").click(function() {
+			$("#view_table").find("tbody").hide();
+			$("#" + $(this).attr("data-view")).show();
+			$(".sub-header").text($(this).attr("data-title") + " Items");
+		});
+	</script>
 </footer>
 </body>
 </html>
