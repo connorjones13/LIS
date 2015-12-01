bc2e4de1d571ccb73d7e6ad00d8717746c28d68b
Connor Jones <connorjones135@gmail.com>
2015-12-01 10:37:12 -0600

    updated footer link

--- html/contact.php
+++ html/contact.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . "/../../includes/LIS/autoload.php");
+require_once(__DIR__ . "/../includes/LIS/autoload.php");
 $pdo = new \LIS\Database\PDO_MySQL();
 $controller = new \LIS\Controllers\BaseController($pdo);
 
@@ -8,18 +8,18 @@ $page_title = "Contact Us";
 <!doctype html>
 <html lang="en">
 <head>
-	<?php require_once(__DIR__ . "/head.php"); ?>
+	<?php require_once(__DIR__ . "/../includes/html_templates/head.php"); ?>
 </head>
 <body>
 <header class="navbar navbar-fixed-top navbar-inverse">
-	<?php require_once(__DIR__ . "/header.php"); ?>
+	<?php require_once(__DIR__ . "/../includes/html_templates/header.php"); ?>
 </header>
 <div class="content">
 	<h1>Contact Us</h1>
 	<p>Have questions? Drop us an <a href="mailto:ski0005@auburn.edu">email</a> and we'll get back to you as soon as possible!</p>
 </div>
 <footer class="footer">
-	<?php require_once(__DIR__ . "/footer.php"); ?>
+	<?php require_once(__DIR__ . "/../includes/html_templates/footer.php"); ?>
 </footer>
 </body>
 </html>
