e8f2359c089fc094f9fc88de00bb9ef7e3f236b0
Steven Imle (MacBook) <stevenimle@gmail.com>
2015-12-01 16:46:03 -0600

    Added a missing method... not pointing fingers

--- includes/LIS/Contact/Mailer.php
+++ includes/LIS/Contact/Mailer.php
@@ -33,4 +33,17 @@
 
 			$this->_mailer->send();
 		}
+
+
+		public function sendAccountConfirmationEmail(User $user) {
+			$this->_mailer->setFrom("no-reply@library.com");
+			$this->_mailer->addAddress($user->getEmail(), $user->getNameFull());
+
+			//TODO: Move this to external loading via html files if there is time.
+			$this->_mailer->msgHTML("Hello " . $user->getNameFirst() . ",<br><br>Please click "
+					. "<a href=\"localhost:8888/account/confirm/?st=" . $user->getAccountConfirmToken() . "\">here</a> "
+					. "to confirm your account.");
+
+			$this->_mailer->send();
+		}
 	}
