672984b3bc4309390368e051520f405e2a4a1112
Tyler Williamson <tylerwil@gmail.com>
2015-12-01 17:49:26 -0600

    added password reset controller

---
+++ includes/LIS/Controllers/PasswordResetController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ski
+ * Date: 11/11/15
+ * Time: 12:09 AM
+ */
+
+namespace LIS\Controllers;
+
+use LIS\Contact\Mailer;
+use LIS\User\User;
+use LIS\Utility;
+
+class PasswordResetController extends BaseController {
+    private static $ERROR_USER_NOT_FOUND = 69;
+
+
+    public function resetPassword($confirmationEmail, $email) {
+        if($confirmationEmail == $email) {
+            $user = User::findByEmail($this->_pdo, $email);
+
+            if(is_null($user)) {
+                //error
+                $this->setError(self::$ERROR_USER_NOT_FOUND);
+            } else {
+                $mailer = new Mailer();
+                $mailer->sendPasswordResetEmail($user);
+
+                $_SESSION["successMessage"] = "Password reset email sent";
+            }
+        }
+    }
+
+    public function getErrorMessage() {
+        switch ($this->getError()) {
+            case self::$ERROR_USER_NOT_FOUND:
+                return "User not found.";
+            default:
+                return false;
+        }
+    }
+
+}
