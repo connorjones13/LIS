bd1f8cb41d7d81b5a63f39b39f027049fc917ca2
Connor Jones <connorjones135@gmail.com>
2015-12-01 10:23:05 -0600

    added manage user view

--- html/Item/index.php
+++ html/Item/index.php
@@ -1,7 +1,7 @@
 <?php
 	require_once(__DIR__ . "/../../includes/LIS/autoload.php");
 	$pdo = new \LIS\Database\PDO_MySQL();
-	$controller = new \LIS\Controllers\CreateUserController($pdo); //todo: why is this createusercontroller?
+	$controller = new \LIS\Controllers\CreateUserController($pdo);
 
 	$item = \LIS\RentalItem\RentalItem::find($pdo, $_GET["id"]);
 
