351249045334f1ec22863552e7e6ad27c6f1ebb6
Connor Jones <connorjones135@gmail.com>
2015-12-03 22:13:52 -0600

    updated nav

--- includes/html_templates/control_panel_nav.php
+++ includes/html_templates/control_panel_nav.php
@@ -6,11 +6,6 @@
 			<a href="/controlpanel/reports/">Reports</a></li>
 		<li <?php if($page_title == 'Add Rental Item') echo 'class="active"'?>>
 			<a href="/controlpanel/add/">Add Rental Item</a></li>
-			<!--<ul class="nav nav-sidebar">
-				<li><a href="/controlpanel/add/index.php">Add Magazine</a></li>
-				<li><a href="/controlpanel/add/index.php">Add Book</a></li>
-				<li><a href="/controlpanel/add/index.php">Add DVD</a></li>
-			</ul> -->
 		<li <?php if($page_title == 'Manage Users') echo 'class="active"'?>>
 			<a href="/controlpanel/users">Manage Users</a></li>
 		<li <?php if($page_title == 'Checkout') echo 'class="active"' ?>>
